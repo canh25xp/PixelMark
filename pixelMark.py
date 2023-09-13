2dbe8738e4160b2834a7bf7b3c992bcc4098d501
canh25xp <canh25xp@gmail.com>
2023-09-13 13:59:23 +0700

    edit source file

--- pixelMark.py
+++ pixelMark.py
@@ -1,10 +1,11 @@
 import cv2 as cv
+import numpy as np
 from pathlib import Path
 
-imgPath = "./input/cccdc_front_1.jpg"
+imgPath = "./input/cccdc_back_2_20.jpg"
 title_window = "Pixel Mark"
 
-coor = list()
+coors = []
 
 index = 0
 
@@ -15,7 +16,7 @@ def mouse_callback(event, x, y, flags, params):
         cv.drawMarker(img, (x,y), (0,0,255), cv.MARKER_CROSS, 5)
         cv.putText(img, str(index), (x,y-5), cv.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255))
         index+=1
-        coor.append((x, y))
+        coors.append((x, y))
         print ((x, y))
         outputFile.write("{} {}\n".format(x,y))
         cv.imshow(title_window, img)
@@ -35,11 +36,17 @@ if __name__ == "__main__":
     cv.setMouseCallback(title_window, mouse_callback)
     
     cv.imshow(title_window, img)
-    cv.waitKey(0)
-    cv.imwrite(str(outputImg), img)
-    outputFile.close()
-    cv.destroyAllWindows()
     
     cv.waitKey()
-        
     cv.destroyAllWindows()
+    
+    for i in range(0, 10):
+        x1, y1 = coors[i]
+        x2, y2 = coors[i + 10]
+        tan = (y2-y1)/(x2-x1)
+        angle = np.rad2deg(np.arctan(tan))
+        print(angle)
+        outputFile.write("{}\n".format(angle))
+    
+    cv.imwrite(str(outputImg), img)
+    outputFile.close()
